9b3d89824eda2fc56e9a87868062cba1710d1412
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2023-04-18 16:41:14 -0700

    Create navBar component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,11 @@
+import createNavBar from '../navBar/navBar.js';
+
 const textStyles = ['text-6xl', 'font-bold'];
 const headerStyles = [
   'mb-4',
   'flex',
+  'flex-col',
+  'gap-4',
   'items-center',
   'justify-center',
   'p-6',
@@ -11,10 +15,12 @@ const headerStyles = [
 function createHeader() {
   const header = document.createElement('header');
   const text = document.createElement('h1');
+  const navBar = createNavBar();
 
   text.textContent = 'BurgerZone';
   text.classList.add(...textStyles);
   header.appendChild(text);
+  header.appendChild(navBar);
 
   header.classList.add(...headerStyles);
   return header;
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,42 @@
+const navBarStyles = ['flex', 'gap-2', 'text-lg', 'font-bold'];
+const tabStyles = {
+  default: [
+    'rounded-sm',
+    'border-b-0',
+    'border-l-2',
+    'border-r-2',
+    'border-t-2',
+    'border-solid',
+    'border-black',
+    'p-2',
+    'hover:bg-gray-300',
+    'active:bg-gray-400',
+  ],
+  active: ['bg-gray-500'],
+};
+
+function createNavBar() {
+  const navBar = document.createElement('nav');
+  const homeTab = document.createElement('button');
+  const menuTab = document.createElement('button');
+  const contactTab = document.createElement('button');
+
+  homeTab.textContent = 'Home';
+  homeTab.classList.add(...tabStyles.default);
+  homeTab.classList.toggle(...tabStyles.active);
+  navBar.appendChild(homeTab);
+
+  menuTab.textContent = 'Menu';
+  menuTab.classList.add(...tabStyles.default);
+  navBar.appendChild(menuTab);
+
+  contactTab.textContent = 'Menu';
+  contactTab.classList.add(...tabStyles.default);
+  navBar.appendChild(contactTab);
+
+  navBar.classList.add(...navBarStyles);
+
+  return navBar;
+}
+
+export default createNavBar;
